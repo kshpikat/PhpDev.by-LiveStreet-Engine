1ffab79edb5da6aaf7fbdebcaf329476dc78fd87
Kanstantsin Shpikat <k.shpikat@intetics.com>
2012-02-03 17:50:06 +0300

    Gravatar plugin

---
+++ blog/plugins/gravatar/classes/modules/gravatar/entity/User.entity.class.php
@@ -0,0 +1,12 @@
+<?php
+class PluginGravatar_ModuleGravatar_EntityUser extends PluginGravatar_Inherit_ModuleUser_EntityUser {
+    
+    public function getProfileAvatarPath($iSize=100) {
+        if ($sPath=$this->getProfileAvatar()) { 	
+        	return str_replace('_100x100',(($iSize==0)?"":"_{$iSize}x{$iSize}"),$sPath."?".date('His',strtotime($this->getProfileDate())));
+    	} else {
+    		return "http://www.gravatar.com/avatar.php? gravatar_id=".md5( strtolower($this->getMail()) )."&size=".$iSize;
+    	}
+    }
+}
+?>
