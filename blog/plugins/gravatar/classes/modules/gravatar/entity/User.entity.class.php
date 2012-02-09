0dbbe4b921b42b5c28680ec6a3bee178bf5a3005
Kanstantsin Shpikat <k.shpikat@intetics.com>
2012-02-09 15:50:39 +0300

    Gravatar fix

--- blog/plugins/gravatar/classes/modules/gravatar/entity/User.entity.class.php
+++ blog/plugins/gravatar/classes/modules/gravatar/entity/User.entity.class.php
@@ -5,7 +5,7 @@ class PluginGravatar_ModuleGravatar_EntityUser extends PluginGravatar_Inherit_Mo
         if ($sPath=$this->getProfileAvatar()) { 	
         	return str_replace('_100x100',(($iSize==0)?"":"_{$iSize}x{$iSize}"),$sPath."?".date('His',strtotime($this->getProfileDate())));
     	} else {
-    		return "http://www.gravatar.com/avatar.php? gravatar_id=".md5( strtolower($this->getMail()) )."&size=".$iSize;
+    		return "http://www.gravatar.com/avatar/".md5( strtolower($this->getMail()) )."?s=".$iSize"&d=monsterid";
     	}
     }
 }
