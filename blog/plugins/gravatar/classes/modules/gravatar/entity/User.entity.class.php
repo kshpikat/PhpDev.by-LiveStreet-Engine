48db7e52ae12efa0b87d7a101bca98248355ffb2
Kanstantsin Shpikat <k.shpikat@intetics.com>
2012-02-16 15:24:46 +0300

    Remove param to prevent avator reloading from twitter, facebook and other services

--- blog/plugins/gravatar/classes/modules/gravatar/entity/User.entity.class.php
+++ blog/plugins/gravatar/classes/modules/gravatar/entity/User.entity.class.php
@@ -3,7 +3,7 @@ class PluginGravatar_ModuleGravatar_EntityUser extends PluginGravatar_Inherit_Mo
     
     public function getProfileAvatarPath($iSize=100) {
         if ($sPath=$this->getProfileAvatar()) { 	
-        	return str_replace('_100x100',(($iSize==0)?"":"_{$iSize}x{$iSize}"),$sPath."?".date('His',strtotime($this->getProfileDate())));
+        	return str_replace('_100x100',(($iSize==0)?"":"_{$iSize}x{$iSize}"),$sPath);
     	} else {
     		return "http://www.gravatar.com/avatar/".md5( strtolower($this->getMail()) )."?s=".$iSize."&d=monsterid";
     	}
